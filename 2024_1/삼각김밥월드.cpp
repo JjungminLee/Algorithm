eda1c1c6186f51e797c4b93f9d0568bfe1edc722
JjungminLee <julie0964@naver.com>
2024-01-23 01:58:12 +0900

    240122끄적

--- 2024_1/삼각김밥월드.cpp
+++ 2024_1/삼각김밥월드.cpp
@@ -2,117 +2,53 @@
 #include <vector>
 #include<algorithm>
 using namespace std;
-
-vector<int> v;
+int d[10001];
 int T;
+void init(){
+    int cnt = 0;
+    for(int i=1;i<12;i+=cnt){
+       for(int j=i;j<=i+cnt;j++){
+        
+         d[j]=cnt+1;
+       }
+        cnt++;
+    }
+    d[1]=1;
+}
 
 int main(){
 
-    ios::sync_with_stdio(0);
     cin>>T;
-    int targetNum=1;
-    int step =0;
-    while(true){
-        if(targetNum>=10000){
-            break;
-        }else{
-            targetNum+=step;
-            v.push_back(targetNum);
-            step++;
-
-        }
-    }
     for(int i=0;i<T;i++){
         int s,e;
         cin>>s>>e;
-        if(s==e){
-            cout<<"#"<<i+1<<" "<<0<<endl;
-         
-        }else{
-            int startIdx=0;
-            int endIdx=0;
-            for(int j=0;j<v.size();j++){
-                if(s<=v[j]){
-                   if(s==v[j]){
-                     startIdx=j+1;
-                   
-                   }else{
-                    startIdx=j;
-                   }
-                   break;
-
-                }
+        if(s>e){
+            swap(s,e);
+        }
+        init();
+        int sR=s;
+        int sL=s;
+        int cntR=0;
+        int cntL=0;
+        while(true){
+              
+            //같은 레벨일때
+            if(d[sR]==d[e]|| d[sL]==d[e]){
+                cntR+=abs(e-sR);
+                cntL+=abs(e-sL);
+                break;
             }
-            for(int j=0;j<v.size();j++){
-                if(e<=v[j]){
-                    if(e==v[j]){
-                        endIdx=j+1;
-                   
-                   }else{
-                        endIdx=j;
-                   }
-                   break;
-
-                }
+            else{
+                sL+=d[s];
+                sR+=d[s]+1;
+                cntR++;
+                cntL++;
+                
             }
-    
-            if(abs(startIdx-endIdx)==0){
-                // 같은 레벨에서 이동
-                cout<<"#"<<i+1<<" "<<abs(s-e)<<endl;
-            }else{
-                //다른 레벨에서 이동
-                // 레벨 이동 경계값 계산
-                int minIdx = min(startIdx,endIdx);
-                int maxIdx = max(startIdx,endIdx);
-                int startNum = min(s,e);
-                int endNum = max(s,e);
-            
-                // 왼쪽 끝에서 오른쪽 끝으로 가는 경우
-                if(startNum==v[minIdx-1]&&endNum==v[maxIdx]-1){
-                   if(maxIdx-minIdx==1){
-                     cout<<"#"<<i+1<<" "<<v[maxIdx-1]-v[minIdx-1]<<endl;
-                   }else{
-                     cout<<"#"<<i+1<<" "<<maxIdx-minIdx+1<<endl;
-                   }
-
-                }else if(startNum==v[minIdx-1] && endNum==v[maxIdx-1]){
-                       // 왼쪽 끝에서 왼쪽 끝으로 가는 경우
-                    cout<<"#"<<i+1<<" "<<maxIdx-minIdx<<endl;
-                }else if(startNum==v[minIdx]-1 && endNum==v[maxIdx]-1){
-                     // 오른쪽 끝에서 오른쪽 끝으로 가는 경우
-                    cout<<"#"<<i+1<<" "<<maxIdx-minIdx<<endl;
-                }else if(startNum==v[minIdx]-1&& endNum==v[maxIdx-1]){
-                        // 오른쪽 끝에서 왼쪽 끝으로 가는 경우
-                    if(maxIdx-minIdx==1){
-                     cout<<"#"<<i+1<<" "<<v[maxIdx-1]-v[minIdx-1]<<endl;
-                   }else{
-                     cout<<"#"<<i+1<<" "<<maxIdx-minIdx+1<<endl;
-                   }
-
-                }else if(startNum == v[minIdx]-1 || endNum == v[maxIdx]-1){
-                    // 오른쪽 끝으로 가는 경우
-                    cout<<"#"<<i+1<<" "<<maxIdx-minIdx+1<<endl;
-
-                }else if(startNum == v[minIdx-1] || endNum == v[maxIdx-1]){
-                    // 왼쪽 끝으로 가는 경우
-                    cout<<"#"<<i+1<<" "<<maxIdx-minIdx+1<<endl;
-
-                }
-                else{
-                    cout<<"#"<<i+1<<" "<<maxIdx-minIdx<<endl;
-
-                }
-
-            
-        
-            }
-    
 
         }
+        cout<<min(cntR,cntL)<<endl;
 
     }
 
-  
-
-    return 0;
 }

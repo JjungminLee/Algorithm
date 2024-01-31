0bd99b17a70f7c6809c91dc65cfd27e336e1b556
JjungminLee <julie0964@naver.com>
2024-01-31 15:35:23 +0900

    240131

--- 2024_1/tempCodeRunnerFile.cpp
+++ 2024_1/tempCodeRunnerFile.cpp
@@ -1,4 +1,1 @@
-    cout<<"nx"<<nx<<"ny "<<ny<<endl;
-                        cout<<"x "<<x<<"y "<<y<<endl;
-                        ans = min(ans,abs(x-nx)+abs(y-ny)-1);
-                        cout<<"ans: "<<ans<<endl;
+     // exit(0);

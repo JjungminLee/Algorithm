6354e8f5d2fbb24198d956829eaf3ec43f2cd29c
JjungminLee <julie0964@naver.com>
2024-02-10 05:38:47 +0900

    240209보완

--- 2024_02/Boj_17140.cpp
+++ 2024_02/Boj_17140.cpp
@@ -13,7 +13,7 @@ void colCompare(){
         같다면 두 번째 인자 기준으로 오름차순 정렬*/
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         vector<int>v;
-        int nRowCnt=1;
+        int nRowCnt=0;
         for(int j=1;j<=rowCnt;j++){
             int std = arr[j][i];
             bool flag=true;
@@ -25,36 +25,46 @@ void colCompare(){
                 }
             }
             if(flag){
-                v.push_back(std);
-                int cnt=0;
-                for(int k=1;k<=rowCnt;k++){
-                        if(std==arr[k][i]){
-                            cnt++;
-                        }
+                if(std!=0){
+                    v.push_back(std);
+                    int cnt=0;
+                    for(int k=1;k<=rowCnt;k++){
+                            if(std==arr[k][i]){
+                                cnt++;
+                            }
+                    }
+                    // 횟수,숫자
+                    pq.push({cnt,std});
                 }
-                // 횟수,숫자
-                pq.push({cnt,std});
-            }
-             cout<<"col"<<endl;
-            for(int k=0;k<v.size();k++){
-                cout<<v[k]<<" ";
-            }
-            while(!pq.empty()){
+                
+            }   
+
+        }
+        // cout<<"col"<<endl;
+        // for(int k=0;k<v.size();k++){
+        //         cout<<v[k]<<" ";
+        // }
+        while(!pq.empty()){
                 int num = pq.top().second;
                 int numCnt = pq.top().first;
                 // cout<<"col"<<endl;
                 // cout<<"num: "<<num<<"numCnt: "<<numCnt<<endl;
                 pq.pop();
-                arr[i][nRowCnt]=num;
-                nRowCnt+=1;
-                arr[i][nRowCnt]=numCnt;
-                nRowCnt+=1;
-
-            }
-            rowCnt=max(rowCnt,nRowCnt);
-            
+                nRowCnt++;
+                arr[nRowCnt][i]=num;
+                nRowCnt++;
+                arr[nRowCnt][i]=numCnt;
+                
 
         }
+        if(rowCnt>nRowCnt){
+            for(int j=nRowCnt+1;j<=rowCnt;j++){
+                arr[j][i]=0;
+            }
+        }
+       
+        rowCnt=max(rowCnt,nRowCnt);
+     
        
     }
 
@@ -66,7 +76,7 @@ void rowCompare(){
         같다면 두 번째 인자 기준으로 오름차순 정렬*/
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         vector<int>v;
-        int nColCnt=1;
+        int nColCnt=0;
         for(int j=1;j<=colCnt;j++){
             int std = arr[i][j];
             bool flag=true;
@@ -78,37 +88,46 @@ void rowCompare(){
                 }
             }
             if(flag){
-                v.push_back(std);
-                int cnt=0;
-                for(int k=1;k<=colCnt;k++){
-                        if(std==arr[i][k]){
-                            cnt++;
-                        }
+                if(std!=0){
+                    v.push_back(std);
+                    int cnt=0;
+                    for(int k=1;k<=colCnt;k++){
+                            if(std==arr[i][k]){
+                                cnt++;
+                            }
+                    }
+                    // 횟수,숫자
+                    pq.push({cnt,std});
                 }
-                // 횟수,숫자
-                pq.push({cnt,std});
+               
             }
-            cout<<"row"<<endl;
-            for(int k=0;k<v.size();k++){
-                cout<<v[k]<<" ";
-            }
-            cout<<endl;
-            while(!pq.empty()){
-                int num = pq.top().second;
-                int numCnt = pq.top().first;
-                // cout<<"row"<<endl;
-                // cout<<"num: "<<num<<"numCnt: "<<numCnt<<endl;
-                pq.pop();
-                arr[i][nColCnt]=num;
-                nColCnt+=1;
-                arr[i][nColCnt]=numCnt;
-                nColCnt+=1;
-
-            }
-            colCnt=max(colCnt,nColCnt);
-            
+                
 
         }
+        // cout<<"row"<<endl;
+        // for(int k=0;k<v.size();k++){
+        //         cout<<v[k]<<" ";
+        // }
+        // cout<<endl;
+        while(!pq.empty()){
+                int num = pq.top().second;
+                int numCnt = pq.top().first;
+                pq.pop();
+                
+                nColCnt++;
+                arr[i][nColCnt]=num;
+                nColCnt++;
+                arr[i][nColCnt]=numCnt;
+
+        }
+        if(colCnt>nColCnt){
+            for(int j=nColCnt+1;j<=colCnt;j++){
+                arr[i][j]=0;
+            }
+        }
+      
+       
+        colCnt=max(colCnt,nColCnt);
        
     }
     
@@ -127,6 +146,11 @@ int main(){
         }
         if(ans>100){
             cout<<-1<<endl;
+            exit(0);
+            break;
+            
+        }
+        if(rowCnt>=100|colCnt>=100){
             break;
         }
         ans++;
@@ -135,15 +159,20 @@ int main(){
         }else{
             colCompare();
         }
-        cout<<"ans: "<<ans<<endl;
-        cout<<"rowCnt: "<<rowCnt<<"colCnt: "<<colCnt<<endl;
-        for(int i=1;i<=rowCnt;i++){
-            for(int j=1;j<=colCnt;j++){
-                cout<<arr[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-        cout<<endl;
+        // cout<<"ans: "<<ans<<endl;
+        // cout<<"rowCnt: "<<rowCnt<<"colCnt: "<<colCnt<<endl;
+       
+        // for(int i=1;i<=rowCnt;i++){
+       
+        //     for(int j=1;j<=colCnt;j++){
+        //         cout<<arr[i][j]<<" ";
+               
+        //     }
+    
+        //     cout<<endl;
+        // }
+        
+ 
     }
     cout<<ans<<endl;
 
--- 2024_02/tempCodeRunnerFile.cpp
+++ 2024_02/tempCodeRunnerFile.cpp
@@ -1,5 +1,9 @@
-    cout<<"vector**"<<endl;
-    for(int i=0;i<v.size();i++){
-        cout<<v[i]<<" ";
-    }
-    cout<<endl;
+cout<<"ans: "<<ans<<endl;
+        cout<<"rowCnt: "<<rowCnt<<"colCnt: "<<colCnt<<endl;
+        for(int i=1;i<=rowCnt;i++){
+            for(int j=1;j<=colCnt;j++){
+                cout<<arr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;

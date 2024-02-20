b421312c6e0a2ca933a0f5617675c74ba6bef906
JjungminLee <julie0964@naver.com>
2024-02-21 00:02:58 +0900

    fix: 코드수정

--- 2024_02/Boj_1904.cpp
+++ 2024_02/Boj_1904.cpp
@@ -2,17 +2,13 @@
 
 using namespace std;
 int N;
-int dp[10000000];
+long long dp[10000000];
 int main(){
     cin>>N;
     dp[1]=1;
     dp[2]=2;
     for(int i=3;i<=N;i++){
-        if(i%2==0){
-            dp[i]=2*dp[i-2]-1+dp[i-1]-1;
-        }else{
-            dp[i]=2*dp[i-2]+dp[i-1]-1;
-        }
+        dp[i]=(dp[i-1]+dp[i-2])%15746;
     }
-    cout<<dp[N]%15746<<endl;
+    cout<<dp[N]<<endl;
 }

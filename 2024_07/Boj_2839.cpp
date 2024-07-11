7a4b8aa66ecad4d25f71be6f48abc41b0b34f4a3
JjungminLee <julie0964@naver.com>
2024-07-11 23:56:44 +0900

    240711

---
+++ 2024_07/Boj_2839.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int ans;
+int main()
+{
+    cin >> n;
+
+    while (n >= 0)
+    {
+        if (n % 5 == 0)
+        {
+
+            ans += (n / 5);
+            cout << ans << endl;
+            return 0;
+        }
+        n -= 3;
+        ans++;
+    }
+    cout << -1;
+}
--- 2024_07/tempCodeRunnerFile.cpp
+++ 2024_07/tempCodeRunnerFile.cpp
@@ -1,11 +1,32 @@
-  for (int i = 0; i < v.size(); i++)
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int ans;
+int main()
+{
+    cin >> n;
+    while (1)
+    {
+        if (n == 0)
         {
-            if (v[i].first == input)
-            {
-                v[i].second += 1;
-            }
-            else
-            {
-                v.push_back({input, 1});
-            }
+            cout << ans << endl;
+            break;
         }
+        else if ((n - 5) % 5 == 0 || (n - 5) % 3 == 0)
+        {
+            n -= 5;
+            ans++;
+        }
+        else if ((n - 3) % 5 == 0 || (n - 3) % 3 == 0)
+        {
+            n -= 3;
+            ans++;
+        }
+        else
+        {
+            ans = -1;
+            cout << ans << endl;
+            break;
+        }
+    }
+}

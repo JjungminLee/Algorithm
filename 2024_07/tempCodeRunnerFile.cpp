6a9d38c01206c7667c5cb95f4717a5c291b2f45d
JjungminLee <julie0964@naver.com>
2024-07-15 23:57:38 +0900

    240716

--- 2024_07/tempCodeRunnerFile.cpp
+++ 2024_07/tempCodeRunnerFile.cpp
@@ -1,1 +1,19 @@
-    // answer = ans[0];
+ // for (int i = 0; i < n; i++)
+    // {
+    //     int flag = 1;
+    //     int std = arr[i][0];
+
+    //     for (int j = 1; j < n; j++)
+    //     {
+
+    //         if (arr[i][j] != std)
+    //         {
+    //             flag = 0;
+    //         }
+    //     }
+
+    //     if (flag == 1)
+    //     {
+    //         ans++;
+    //     }
+    // }

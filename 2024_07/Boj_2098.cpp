091bc13ef1aced117c139f848316b69aad8a3f01
JjungminLee <julie0964@naver.com>
2024-07-17 17:26:44 +0900

    외판원 순회 다시풀어보기

--- 2024_07/Boj_2098.cpp
+++ 2024_07/Boj_2098.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define INF 987654321;
+int intMax = INT_MAX - 1;
+#define INF intMax;
 int n;
 int arr[16][16];
-int dp[16][1 << 16]; // dp[pos][visited] 현재 pos 위치에 있고, visited 상태일 때 최소 비용
-
+int dp[16][1 << 16];
 int tsp(int pos, int bit)
 {
-    // 만약 비트 마스킹이 다 채워지면 0으로 돌아가기
-    // n이 4하면 16-1 = 15 1111 이 됨!
+    // bit 마스킹 전부다 방문한거면 다시 원점으로 돌아오게
     if (bit == (1 << n) - 1)
     {
-        // 길이 없다면
         if (arr[pos][0] == 0)
         {
             return INF;
@@ -19,29 +17,25 @@ int tsp(int pos, int bit)
         return arr[pos][0];
     }
 
-    // 이미 있으면 그거 리턴
+    // 이미 dp 값이 있다면 그거 리턴
     if (dp[pos][bit] != -1)
     {
         return dp[pos][bit];
     }
 
-    // min값 찾는거기에 이거 처리하는거 중요!
     dp[pos][bit] = INF;
-    // 방문하지 않은 도시들 체크 하면서 방문해보기!
+    // 그게 아니면 방문안한 도시 다 방문해보기
     for (int i = 0; i < n; i++)
     {
-        // 길이 없다용!
         if (arr[pos][i] == 0)
         {
             continue;
         }
-        // 방문여부 확인
         if ((bit & (1 << i)) == (1 << i))
         {
             continue;
         }
-        // 방문하지 않은 곳이라면 방문 한거 추가해야!
-        dp[pos][bit] = min(dp[pos][bit], arr[pos][i] + tsp(i, bit | (1 << i)));
+        dp[pos][bit] = min(dp[pos][bit], arr[pos][i] + tsp(i, bit | 1 << i));
     }
     return dp[pos][bit];
 }

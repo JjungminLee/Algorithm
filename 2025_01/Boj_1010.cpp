0659afa77568972079a8539b564614ab2194c81f
JjungminLee <julie0964@naver.com>
2025-01-12 00:17:18 +0900

    250112

---
+++ 2025_01/Boj_1010.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int t;
+int ans;
+int combination(int n, int r)
+{
+    int res = 1;
+    if (n - r < r)
+    {
+        r = n - r;
+    }
+
+    for (int i = 0; i < r; i++)
+    {
+        res = ((res * (n - i)) / (i + 1));
+    }
+    return res;
+}
+int main()
+{
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        int ans = combination(b, a);
+        cout << ans << endl;
+    }
+}

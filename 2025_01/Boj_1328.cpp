f75ac3350f338d970f843485d91be0193887379c
JjungminLee <julie0964@naver.com>
2025-01-16 17:33:17 +0900

    250116

---
+++ 2025_01/Boj_1328.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int l;
+int r;
+long long dp[101][101][101];
+int MOD = 1000000007;
+int main()
+{
+    cin >> n >> l >> r;
+    dp[1][1][1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            for (int k = 1; k <= i; k++)
+            {
+                dp[i][j][k] += (dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] + dp[i - 1][j][k] * (i - 2)) % MOD;
+            }
+        }
+    }
+
+    cout << dp[n][l][r] << endl;
+}

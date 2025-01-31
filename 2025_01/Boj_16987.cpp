1509431435ae48b8ea5e85d5dd34d4424ddbc908
JjungminLee <julie0964@naver.com>
2025-01-31 18:39:38 +0900

    250131

--- 2025_01/Boj_16987.cpp
+++ 2025_01/Boj_16987.cpp
@@ -2,28 +2,58 @@
 using namespace std;
 
 int n;
+// 내구도, 무게
 vector<pair<int, int>> v;
+int ans;
+// 인덱스 , 현재 깬 개수
+void backTracking(int depth, int cnt)
+{
+
+    if (depth == n)
+    {
+        ans = max(cnt, ans);
+        return;
+    }
+    // 내구도
+
+    if (v[depth].first <= 0)
+    {
+        backTracking(depth + 1, cnt);
+        return;
+    }
+    // 한번이라도 계란을 친적이 있는지
+    bool hit = false;
+    for (int i = 0; i < n; i++)
+    {
+        // 이미 깨졌거나 자기자신은 치면 안됨
+        if (v[i].first <= 0 || i == depth)
+        {
+            continue;
+        }
+        v[depth].first -= v[i].second;
+        v[i].first -= v[depth].second;
+        hit = true;
+        // 깨졌을때만 cnt+1이고, 왼쪽,오른쪽이 같이 깨질 수 있음을 고려해야함!
+        backTracking(depth + 1, cnt + (v[depth].first <= 0) + (v[i].first <= 0));
+        v[depth].first += v[i].second;
+        v[i].first += v[depth].second;
+    }
+    if (!hit)
+    {
+        backTracking(depth + 1, cnt);
+    }
+}
 int main()
 {
     cin >> n;
     for (int i = 0; i < n; i++)
     {
         int a, b;
+        // 내구도 무게
         cin >> a >> b;
         v.push_back({a, b});
     }
 
-    for (int i = 0; i < n; i++)
-    {
-        int left = v[i].first;
-        int leftWeight = v[i].second;
-        for (int j = i + 1; j < n; j++)
-        {
-            int right = v[j].first;
-            int rightWeight = v[j].first;
-
-            // int brokenLeft = v[i].second-ㄹ
-            // dd
-        }
-    }
+    backTracking(0, 0);
+    cout << ans << endl;
 }

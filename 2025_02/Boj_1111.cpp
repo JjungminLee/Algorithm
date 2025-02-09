638fc5b57b7b6fcb34ffd42e78ee01b1e88c4b0a
JjungminLee <julie0964@naver.com>
2025-02-09 23:58:49 +0900

    250209

---
+++ 2025_02/Boj_1111.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+vector<int> v;
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        v.push_back(num);
+    }
+    int a, b = 0;
+    if (v[1] - v[0] == 0)
+    {
+        a = 0;
+        b = v[1];
+    }
+    else
+    {
+        a = (v[2] - v[1]) / (v[1] - v[0]);
+        b = v[2] - v[1] * a;
+    }
+
+    bool flag = true;
+    for (int i = 0; i < n - 1; i++)
+    {
+        int first = v[i];
+        int second = v[i + 1];
+        if (first * a + b != second)
+        {
+            flag = false;
+        }
+    }
+    if (v.size() == 1)
+    {
+        cout << "A" << endl;
+    }
+    else if (v.size() == 2)
+    {
+        if (v[0] == v[1])
+        {
+            cout << v[1] << endl;
+        }
+        else
+        {
+            cout << "B" << endl;
+        }
+    }
+    else
+    {
+        if (flag)
+        {
+            cout << v[v.size() - 1] * a + b << endl;
+        }
+        else
+        {
+            cout << "B" << endl;
+        }
+    }
+}

f859b0072f07c53cf3d92f5829af9ba0b9e4537a
JjungminLee <julie0964@naver.com>
2025-02-14 21:27:29 +0900

    250214

--- 2025_02/Boj_20055.cpp
+++ 2025_02/Boj_20055.cpp
@@ -40,25 +40,25 @@ int main()
     {
         cin >> arr[1][n - i - 1];
     }
-    robot[0][0] = true;
-    ans++;
+
     while (1)
     {
         ans++;
         int rotArr[2][101];
-        bool rotRobot[2][101];
+        bool rotRobot[2][101] = {false};
 
+        /* 벨트 회전*/
         // 0행 0열 ~ 0행 i-2열 까지는 -> 열+1
         // 0행 i-1열은 -> 1행 i-1열로
         // 1행0열은 -> 0행 0열로
         // 1행 i-1열부터 1열은 -> 열-1
         rotArr[1][n - 1] = arr[0][n - 1];
-        for (int i = 1; i < n - 1; i++)
+        for (int i = 1; i < n; i++)
         {
             rotArr[0][i] = arr[0][i - 1];
         }
         rotArr[0][0] = arr[1][0];
-        for (int i = n - 2; i >= 1; i--)
+        for (int i = n - 1; i >= 1; i--)
         {
             rotArr[1][i - 1] = arr[1][i];
         }
@@ -69,18 +69,31 @@ int main()
                 arr[i][j] = rotArr[i][j];
             }
         }
-
+        /*로봇 회전*/
         for (int i = 1; i < n; i++)
         {
-            if (arr[0][i] > 0 && !rotRobot[0][i])
-            {
-                rotRobot[0][i] = robot[0][i - 1];
-                arr[0][i]--;
-            }
+            rotRobot[0][i] = robot[0][i - 1];
         }
-
+        for (int i = 1; i < n; i++)
+        {
+            robot[0][i] = rotRobot[0][i];
+        }
         robot[0][0] = false;
         robot[0][n - 1] = false;
+
+        /*로봇이동*/
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (arr[0][i + 1] > 0 && !robot[0][i + 1] && robot[0][i])
+            {
+                robot[0][i + 1] = true;
+                robot[0][i] = false;
+                arr[0][i + 1]--;
+            }
+        }
+        robot[0][n - 1] = false;
+
         if (arr[0][0] > 0)
         {
             robot[0][0] = true;

0b07b7255f80bfd18e7841ec766653581ee893d6
JjungminLee <julie0964@naver.com>
2025-02-23 20:21:12 +0900

    250223

--- 2025_02/Boj_11053.cpp
+++ 2025_02/Boj_11053.cpp
@@ -1,36 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
-int lis[1000];
-int arr[1000];
-int lds[1000];
+int arr[1001];
+int dp[1001];
+
 int main()
 {
     cin >> n;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
-        lis[i] = 1;
-        lds[i] = 1;
+        dp[i] = 1;
     }
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < i; j++)
         {
             if (arr[i] > arr[j])
             {
-                lis[i] = max(lis[i], lis[j] + 1);
-            }
-            if (arr[i] < arr[j])
-            {
-                lds[i] = max(lds[i], lds[j] + 1);
+                dp[i] = max(dp[i], dp[j] + 1);
             }
         }
     }
     int ans = 0;
     for (int i = 0; i < n; i++)
     {
-        ans = max(lis[i] + lds[i], ans);
+        ans = max(ans, dp[i]);
     }
-    cout << ans + 1 << endl;
+    cout << ans << endl;
 }

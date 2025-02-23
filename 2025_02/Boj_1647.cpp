a9c72dfe43a70e1fb099c1cc90186286ad2a2102
JjungminLee <julie0964@naver.com>
2025-02-24 04:33:28 +0900

    250224

---
+++ 2025_02/Boj_1647.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+int parents[100001];
+vector<pair<int, pair<int, int>>> v;
+int totalCnt;
+int maxCnt;
+
+int getRoot(int num)
+{
+    if (parents[num] == num)
+    {
+        return num;
+    }
+    return parents[num] = getRoot(parents[num]);
+}
+
+void unionParent(int a, int b)
+{
+    int aRoot = getRoot(a);
+    int bRoot = getRoot(b);
+    if (aRoot < bRoot)
+    {
+        parents[bRoot] = aRoot;
+    }
+    else
+    {
+        parents[aRoot] = bRoot;
+    }
+}
+
+bool find(int a, int b)
+{
+    int aRoot = getRoot(a);
+    int bRoot = getRoot(b);
+    if (aRoot == bRoot)
+    {
+        return true;
+    }
+    return false;
+}
+int main()
+{
+    cin >> n >> m;
+    for (int i = 0; i <= n; i++)
+    {
+        parents[i] = i;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        v.push_back({c, {a, b}});
+    }
+    sort(v.begin(), v.end());
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        c = v[i].first;
+        a = v[i].second.first;
+        b = v[i].second.second;
+
+        if (!find(a, b))
+        {
+            unionParent(a, b);
+            totalCnt += c;
+            maxCnt = max(maxCnt, c);
+        }
+    }
+
+    cout << totalCnt - maxCnt << endl;
+}

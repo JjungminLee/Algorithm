bf7e952a252e8c84c9bd2d96825c87f52162fc93
JjungminLee <julie0964@naver.com>
2025-07-31 23:21:23 +0900

    250731: 공격

---
+++ 2025_07/Boj_1430.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int x;
+    int y;
+    double power;
+};
+
+double powers[51];
+int n;
+double r,d;
+int x,y;
+vector<Node>v;
+
+bool calcDist(int x1,int y1,int x2,int y2){
+    return (sqrt(pow(x1-x2,2)+pow(y1-y2,2)))<=r;
+}
+
+
+int main(){
+    cin>>n;
+    cin>>r>>d>>x>>y;
+    for(int i=0;i<n;i++){
+        int a,b;
+        cin>>a>>b;
+        v.push_back({a,b,d});
+    }
+
+    queue<Node>q;
+
+    for(int i=0;i<n;i++){
+        if(calcDist(x,y,v[i].x,v[i].y)){
+            q.push({v[i].x,v[i].y,d});
+            powers[i]=d;
+        }
+    }
+
+    double ans=0;
+
+    while(!q.empty()){
+        Node cur = q.front();
+        q.pop();
+        ans=max(ans,cur.power);
+
+        for(int i=0;i<n;i++){
+            if(calcDist(cur.x,cur.y,v[i].x,v[i].y)){
+                double newPower = (cur.power/2.0);
+                if(powers[i]<newPower){
+                    powers[i]=newPower;
+                    q.push({v[i].x,v[i].y,newPower});
+                }
+            }
+        }
+    }
+    double total=0;
+    for(int i=0;i<n;i++){
+       total+=powers[i];
+    }
+    cout<<total<<endl;
+
+   
+
+
+}

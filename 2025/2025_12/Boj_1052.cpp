1533cd63d2c363b9f927c4ee6b601c2575290206
JjungminLee <julie0964@naver.com>
2025-12-09 09:52:43 +0900

    251209: 물약

--- 2025/2025_12/Boj_1052.cpp
+++ 2025/2025_12/Boj_1052.cpp
@@ -4,7 +4,39 @@ using namespace std;
 long long n;
 long long k;
 
+long long oneCount(long long num){
+    long long cnt = 0L;
+    while(1){
+        if(num==1){
+            cnt++;
+            break;
+        }
+        if(num%2==0){
+            num/=2;
+        }else{
+            cnt++;
+            num/=2;
+        }
+    }
+    return cnt;
+}
+
 int main(){
     cin>>n>>k;
+    long long tot = n;
+    /*
+    tot를 늘려가면서 k개 이하로 1의 개수가 나오는 지점이 물병의
+    최소값을 만들어낼 수 있는 것!
+    */
+   while(1){
+    long long cnt = oneCount(tot);
+    if(cnt<=k){
+        break;
+    }else{
+        tot++;
+    }
+   }
+   cout<<tot-n<<"\n";
+
 
 }

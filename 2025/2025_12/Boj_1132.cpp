5f42cb9149a2ebc3ab27f5773a69eab063ba6384
JjungminLee <julie0964@naver.com>
2025-12-10 15:53:22 +0900

    251210: 합

--- 2025/2025_12/Boj_1132.cpp
+++ 2025/2025_12/Boj_1132.cpp
@@ -3,53 +3,87 @@
 using namespace std;
 
 int n;
-map<char,int>mp;
+// {포인트, 등장횟수}
+map<char,pair<long long,int>>mp;
 vector<string>v;
-vector<pair<int,char>> vp;
+// {포인트, 등장횟수}
+vector<pair<pair<long long,int>,char>> vp;
 map<char,int>mp_point;
-long long ans;
+long long ans=0L;
 
-bool compares(pair<int,char>a,pair<int,char>b){
-    return a.first>b.first;
+bool compares(pair<pair<long long,int>,char>a,pair<pair<long long,int>,char>b){
+    if(a.first.first==b.first.first){
+        if(a.first.second==b.first.second){
+            return a.first<b.first;
+        }
+        return a.first.second>b.first.second;
+    }
+    return a.first.first>b.first.first;
 }
 
 int main(){
 
     cin>>n;
+    bool leading[256] = {false};
     for(int i=0;i<n;i++){
         string str;
         cin>>str;
         v.push_back(str);
+
+        leading[(unsigned char)str[0]] = true;  
         int size = str.size();
         for(int j=0;j<size;j++){
             char ch = str[j];
-            int point = size-j;
-            mp[ch]+=point;
+            long long point = pow(10, size - j - 1);
+            mp[ch].first+=point;
+            mp[ch].second+=1;
         }
     }
 
     for(auto it=mp.begin();it!=mp.end();it++){
-        vp.push_back({it->second,it->first});
+        pair<long long,int> p = it->second;
+        //cout<<"first "<<it->first<<" p "<<p.first<<" "<<p.second<<endl;
+        vp.push_back({{p.first,p.second},it->first});
     }
     sort(vp.begin(),vp.end(),compares);
 
     int curNum = 9;
+    char zeroCandidate = 0;
 
-    for(int i=0;i<vp.size();i++){
-        mp_point[vp[i].second] = curNum-i;
+    if(vp.size()>=10){
+        for(int i = vp.size()-1; i>=0; i--){
+            char c = vp[i].second;
+            if(!leading[(unsigned char)c]){
+                zeroCandidate = c;
+                break;
+            }
+        }
+        for(int i=0;i<vp.size();i++){
+            char c = vp[i].second;
+            if(c == zeroCandidate) continue;  
+            mp_point[c] = curNum;
+            curNum--;
+        }
+        mp_point[zeroCandidate] = 0;
+    }else{
+        for(int i=0;i<vp.size();i++){
+            char c = vp[i].second;
+            mp_point[c] = curNum;
+            curNum--;
+        }
+
     }
 
-    for(auto it=mp_point.begin();it!=mp_point.end();it++){
-        cout<<it->first<<" "<<it->second<<endl;
-    }
+    // for(auto it=mp_point.begin();it!=mp_point.end();it++){
+    //     cout<<it->first<<" "<<it->second<<endl;
+    // }
 
     for(string str:v){
-        int size = str.size();
-        long long curMul = pow(10,size-1);
-        for(int i=0;i<size;i++){
-            ans+=(curMul*mp_point[str[i]]);
-            curMul/=10;
+        long long cur = 0;
+        for(char ch:str){
+            cur = cur*10+mp_point[ch];
         }
+        ans+=cur;
     }
 
     cout<<ans<<"\n";
--- 2025/2025_12/tempCodeRunnerFile.cpp
+++ 2025/2025_12/tempCodeRunnerFile.cpp
@@ -1,3 +1,92 @@
- for(auto it=mp_point.begin();it!=mp_point.end();it++){
-        cout<<it->first<<" "<<it->second<<endl;
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n;
+// {포인트, 등장횟수}
+map<char,pair<long long,int>>mp;
+vector<string>v;
+// {포인트, 등장횟수}
+vector<pair<pair<long long,int>,char>> vp;
+map<char,int>mp_point;
+long long ans=0L;
+
+bool compares(pair<pair<long long,int>,char>a,pair<pair<long long,int>,char>b){
+    if(a.first.first==b.first.first){
+        if(a.first.second==b.first.second){
+            return a.first<b.first;
+        }
+        return a.first.second>b.first.second;
     }
+    return a.first.first>b.first.first;
+}
+
+int main(){
+
+    cin>>n;
+    bool leading[256] = {false};
+    for(int i=0;i<n;i++){
+        string str;
+        cin>>str;
+        v.push_back(str);
+
+        leading[(unsigned char)str[0]] = true;  
+        int size = str.size();
+        for(int j=0;j<size;j++){
+            char ch = str[j];
+            long long point = pow(10, size - j - 1);
+            mp[ch].first+=point;
+            mp[ch].second+=1;
+        }
+    }
+
+    for(auto it=mp.begin();it!=mp.end();it++){
+        pair<int,int> p = it->second;
+        //cout<<"first "<<it->first<<" p "<<p.first<<" "<<p.second<<endl;
+        vp.push_back({{p.first,p.second},it->first});
+    }
+    sort(vp.begin(),vp.end(),compares);
+
+    int curNum = 9;
+    char zeroCandidate = 0;
+
+    if(vp.size()>=10){
+        for(int i = vp.size()-1; i>=0; i--){
+            char c = vp[i].second;
+            if(!leading[(unsigned char)c]){
+                zeroCandidate = c;
+                break;
+            }
+        }
+        for(int i=0;i<vp.size();i++){
+            char c = vp[i].second;
+            if(c == zeroCandidate) continue;  
+            mp_point[c] = curNum;
+            curNum--;
+        }
+        mp_point[zeroCandidate] = 0;
+    }else{
+        for(int i=0;i<vp.size();i++){
+            char c = vp[i].second;
+            mp_point[c] = curNum;
+            curNum--;
+        }
+
+    }
+
+    // for(auto it=mp_point.begin();it!=mp_point.end();it++){
+    //     cout<<it->first<<" "<<it->second<<endl;
+    // }
+
+    for(string str:v){
+        long long cur = 0;
+        for(char ch:str){
+            cur = cur*10+mp_point[ch];
+        }
+        ans+=cur;
+    }
+
+    cout<<ans<<"\n";
+
+    
+}

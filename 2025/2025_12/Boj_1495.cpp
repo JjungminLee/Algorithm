7c8e200088eb7362da97a8372587f4823fbdb3ae
JjungminLee <julie0964@naver.com>
2025-12-13 15:51:54 +0900

    251213: 기타리스트

--- 2025/2025_12/Boj_1495.cpp
+++ 2025/2025_12/Boj_1495.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int n,s,m;
 int arr[55];
 bool dp[55][1005];
-
+int ans;
 
 int main(){
     cin>>n>>s>>m;
@@ -13,14 +13,14 @@ int main(){
     }
     dp[0][s]=true;
     for(int i=1;i<=n;i++){
-        for(int v=0;v<m;v++){
+        for(int v=0;v<=m;v++){
             if(dp[i-1][v]){
-                if(v+arr[i]<=m) dp[i][v+arr[i]]=true;
                 if(v-arr[i]>=0) dp[i][v-arr[i]]=true;
+                if(v+arr[i]<=m) dp[i][v+arr[i]]=true;
             }
         }
     }
-    int ans = -1;
+    ans = -1;
     for(int v=m;v>=0;v--){
         if(dp[n][v]){
             ans = v;

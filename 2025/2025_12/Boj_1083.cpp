4f405bbb23991e58d395edae02de616244308dfd
JjungminLee <julie0964@naver.com>
2025-12-25 09:26:29 +0900

    251225: 소트

---
+++ 2025/2025_12/Boj_1083.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n;
+vector<int>a;
+int s;
+int main(){
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int num;
+        cin>>num;
+        a.push_back(num);
+    }
+    cin>>s;
+    int curPos = 0;
+
+    for(int i=0;i<n&&s>0;i++){
+        int best = i;
+
+        // 인접한 것과 교환 가능
+        for(int j=i+1;j<n&&j<=i+s;j++){
+            if(a[j]>a[best]) best=j;
+        }
+
+        // 바로 옆 인접한 것과 스왑
+        for(int j=best;j>i;j--){
+            swap(a[j],a[j-1]);
+        }
+
+        s-=(best-i);
+    }
+   
+
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<"\n";
+
+}
+
+/*
+
+인접한 걸 고려하지 않아 틀린 문제
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n;
+vector<int>a;
+int s;
+int main(){
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int num;
+        cin>>num;
+        a.push_back(num);
+    }
+    cin>>s;
+    int curPos = 0;
+    while(1){
+        if(s==0) break;
+        if(curPos>=n){
+            curPos-=n;
+        }
+        int curNum = a[curPos];
+        int nextPos=0;
+        int nextNum = 0;
+        for(int i=curPos+1;i<n;i++){
+            if(curNum<a[i]){
+                nextPos=i;
+                nextNum = a[i];
+                break;
+            }
+        }
+        a[curPos]=nextNum;
+        a[nextPos]=curNum;
+        curPos+=2;
+        s--;
+        // for(int i=0;i<n;i++){
+        //     cout<<a[i]<<" ";
+        // }
+        // cout<<"\n";
+        // cout<<"========"<<endl;
+        
+
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<"\n";
+
+}
+
+*/
--- 2025/2025_12/tempCodeRunnerFile.cpp
+++ 2025/2025_12/tempCodeRunnerFile.cpp
@@ -1,37 +1,4 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int n;
-vector<string>v;
-map<string,int>mp;
-
-int cnt=0;
-bool compares(string a,string b){
-    return a.size()<b.size();
-}
-int main(){
-
-    cin>>n;
-    for(int i=0;i<n;i++){
-        string str;
-        cin>>str;
-        v.push_back(str);
+for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
     }
-    sort(v.begin(),v.end(),compares);
-
-    for(int i=0;i<v.size();i++){
-        bool isPrefix = false;
-        for(int j=i+1;j<v.size();j++){
-            if(v[j].substr(0,v[i].size())==v[i]){
-                isPrefix=true;
-                break;
-            }
-
-        }
-        if(isPrefix){
-            cnt++;    
-        }
-    }
-    cout<<n-cnt<<"\n";
-
-}
+    cout<<"\n";

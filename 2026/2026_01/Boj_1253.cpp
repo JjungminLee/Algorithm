87a20306e5ceb43ceb19bd883f59251435538e5b
JjungminLee <julie0964@naver.com>
2026-01-03 23:57:57 +0900

    260103: 좋다

---
+++ 2026/2026_01/Boj_1253.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n;
+vector<long long>v;
+int ans;
+
+int main(){
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        long long num;
+        cin>>num;
+        v.push_back(num);
+    }
+
+    sort(v.begin(),v.end());
+    
+    for(int i=0;i<n;i++){
+
+        long long target = v[i];
+        int l =0;
+        int r = n-1;
+
+        while(l<r){
+            if(l==i){
+                l++;
+                continue;
+            }else if(r==i){
+                r--;
+                continue;
+            }
+            long long sum = v[l]+v[r];
+            if(sum==target){
+                ans++;
+                break;
+            }else if(sum<target){
+                l++;
+            }else{
+                r--;
+            }
+        }
+    }
+
+    cout<<ans<<"\n";
+    
+}
